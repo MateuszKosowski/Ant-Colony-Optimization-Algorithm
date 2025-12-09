d01694462a98fa3cbeea680ce19e0158dc1b705f
Mateusz Kosowski <mateuszkosowski03@gmail.com>
2025-12-09 15:54:47 +0100

    Rename Ant.py to ant.py and implement pheromone update logic; add main function for simulation

---
+++ ant.py
@@ -0,0 +1,111 @@
+import math
+from random import random
+
+
+class Ant:
+
+    pheromone_matrix = []
+
+    # classmetod to dekorator, tworzący metodę "statyczną". Działa na zmiennych klasowych a nie obiektach
+    @classmethod
+    def init_pheromone(cls, places):
+        n = len(places.places_list)
+        pheromone_matrix = []
+        for i in range(n):
+            row = []
+            for j in range(n):
+                row.append(1.0)
+            pheromone_matrix.append(row)
+        Ant.pheromone_matrix = pheromone_matrix
+
+    @classmethod
+    def update_pheromone(cls, ants, evaporation_rate=0.5):
+        n = len(cls.pheromone_matrix)
+
+        # Parowanie feromonów
+        for i in range(n):
+            for j in range(n):
+                cls.pheromone_matrix[i][j] *= (1 - evaporation_rate)
+
+        # Dodawanie nowych feromonów
+        for ant in ants:
+            total_distance = sum(ant.distance_travelled)
+
+            pheromone_deposit = 1 / total_distance
+
+            # Aktualizacja dla każdej krawędzi w trasie mrówki
+            visited_list = list(ant.visited_places)
+            for i in range(len(visited_list) - 1):
+                from_place = visited_list[i]
+                to_place = visited_list[i + 1]
+                cls.pheromone_matrix[from_place.number][to_place.number] += pheromone_deposit
+                cls.pheromone_matrix[to_place.number][from_place.number] += pheromone_deposit
+
+    def __init__(self, place, alpha=1.0, beta=1.0, p_random=0.01):
+        self.current_place = place
+        self.visited_places = set()
+        self.distance_travelled = list()
+        self.alpha = alpha
+        self.beta = beta
+        self.p_random = p_random
+
+        self.visited_places.add(place)
+        self.distance_travelled.append(0)
+
+
+    def calculate_distance(self, place):
+        return math.hypot(
+            place.position[0] - self.current_place.position[0],
+            place.position[1] - self.current_place.position[1]
+        )
+
+    def visit(self, place):
+        if place not in self.visited_places:
+            self.visited_places.add(place)
+        distance = self.calculate_distance(place)
+        self.distance_travelled.append(distance)
+        self.current_place = place
+
+    def available_places(self, places):
+        available_places = []
+        for place in places.places_list:
+            if place not in self.visited_places:
+                available_places.append(place)
+        return available_places
+
+    def select_next_place(self, places):
+        available_places = self.available_places(places)
+
+        # Szansa, że mrówka oleje wszystkie zasady i zrobi coś losowo
+        if random() < self.p_random:
+            next_place = random.choice(available_places)
+        else:
+            numerators = []
+
+            for place in available_places:
+                reverse_distance = 1 / self.calculate_distance(place)
+                pheromone_on_path = Ant.pheromone_matrix[self.current_place.number][place.number]
+                numerator = pow(pheromone_on_path, self.alpha) * pow(reverse_distance, self.beta)
+                numerators.append(numerator)
+
+
+            # Metoda ruletki - k to parametr jak dużo obiektów zwrócić, więc bierzemy [0], bo chcemy tylko 1
+            # Dodatkowo metoda ta sama znormalizuje dane (czyli to z tym total i dzielenie każdego numerator)
+            next_place = random.choices(available_places, weights=numerators, k=1)[0]
+
+        return next_place
+
+    def do_move(self, places):
+        next_place = self.select_next_place(places)
+        self.visit(next_place)
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,49 @@
+from random import randint
+
+from ant import Ant
+from places import Places
+
+
+def main():
+
+    file_to_load = "A-n32-k5.txt"
+    number_of_ants = 50
+    ant_alpha = 1.0
+    ant_beta = 2.0
+    p_random = 0.01
+    number_of_iterations = 500
+    evaporation_rate = 0.5
+
+    all_places = Places(file_to_load)
+
+    Ant.init_pheromone(all_places)
+    best_solution = None
+    best_distance = float('inf')
+
+    for iteration in range(number_of_iterations):
+        ants = []
+
+        for i in range(number_of_ants):
+            random_position = all_places.places_list[randint(0, len(all_places.places_list) - 1)]
+            ant = Ant(random_position, ant_alpha, ant_beta, p_random)
+            ants.append(ant)
+
+        for _ in range(len(all_places.places_list) - 1):
+            for ant in ants:
+                ant.do_move(all_places)
+
+        Ant.update_pheromone(ants, evaporation_rate)
+        for ant in ants:
+            if sum(ant.distance_travelled) < best_distance:
+                best_solution = ant.visited_places
+                best_distance = sum(ant.distance_travelled)
+
+        if iteration % 50 == 0:
+            print(f"Iteracja {iteration}: Najlepsza odległość = {best_distance}")
+
+    print(f"\nNajlepsze rozwiązanie: {best_distance}")
+    print(f"Trasa: {best_solution}")
+
+
+if __name__ == '__main__':
+    main()
